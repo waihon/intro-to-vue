f45d330aa13feb5c3cd83c90250f5683ed80df8e
Waihon Yew <yewwaihon@gmail.com>
2019-02-04 16:03:38 +0800

    02: Attribut binding

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ var app = new Vue({
   el: "#app",
   data: {
     product: "Socks",
-    description: "A pair of warm, fuzzy socks"
+    description: "A pair of warm, fuzzy socks",
+    image: "./assets/vmSocks-blue.png",
+    altText: "A pair of socks",
+    vmLink: "https://www.vuemastery.com/courses/intro-to-vue-js/vue-instance"
   }
 });
