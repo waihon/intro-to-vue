036295fddc20bc3e627ceab3623e3006c5fe698b
Waihon Yew <yewwaihon@gmail.com>
2019-02-05 20:12:14 +0800

    06: Class & style binding

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var app = new Vue({
     image: "./assets/vmSocks-blue.png",
     altText: "A pair of socks",
     vmLink: "https://www.vuemastery.com/courses/intro-to-vue-js/vue-instance",
-    inStock: true,
+    inStock: false,
     onSale: true,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
     variants: [
