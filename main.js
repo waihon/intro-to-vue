434b047aef9589fdc7865078908ac72612f30c8d
Waihon Yew <yewwaihon@gmail.com>
2019-02-05 17:38:53 +0800

    05: Event handling

--- main.js
+++ main.js
@@ -12,13 +12,29 @@ var app = new Vue({
     variants: [
       {
         variantId: 2234,
-        variantColor: "green"
+        variantColor: "green",
+        variantImage: "./assets/vmSocks-green.png"
       },
       {
         variantId: 2235,
-        variantColor: "blue"
+        variantColor: "blue",
+        variantImage: "./assets/vmSocks-blue.png"
       }
     ],
-    sizes: ["XS", "S", "M", "L", "XL", "XXL", "XXXL"]
+    sizes: ["XS", "S", "M", "L", "XL", "XXL", "XXXL"],
+    cart: 0
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    removeFromCart() {
+      if (this.cart > 0) {
+        this.cart -= 1;
+      }
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage;
+    }
   }
 });
